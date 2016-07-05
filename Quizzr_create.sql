529dc9bfaebd1f14236dbcd91a131f5833320fa4
Tyfon <benjamin.fonty@gmail.com>
2016-07-05 20:12:53 +0200

    Updated create sql

--- Quizzr_create.sql
+++ Quizzr_create.sql
@@ -1,77 +1,176 @@
--- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2016-07-04 21:59:43.921
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
--- tables
--- Table: Game
-CREATE TABLE Games (
-    id int NOT NULL AUTO_INCREMENT,
-    genre varchar(64) NOT NULL,
-    current_sample int NOT NULL,
-    nb_players int NOT NULL,
-    current_first int NULL,
-    current_second int NULL,
-    current_third int NULL,
-    CONSTRAINT Game_pk PRIMARY KEY (id)
-);
 
--- Table: Games_samples
-CREATE TABLE Games_samples (
-    id int NOT NULL AUTO_INCREMENT,
-    sample_id int NOT NULL,
-    game_id int NOT NULL,
-    CONSTRAINT Games_samples_pk PRIMARY KEY (id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Table: Games_users
-CREATE TABLE Games_users (
-    id int NOT NULL AUTO_INCREMENT,
-    game_id int NOT NULL,
-    user_id int NOT NULL,
-    score int NOT NULL DEFAULT 0,
-    CONSTRAINT Games_users_pk PRIMARY KEY (id)
-);
+--
+-- Base de données :  `quizzr_db`
+--
 
--- Table: Sample
-CREATE TABLE Samples (
-    id int NOT NULL AUTO_INCREMENT,
-    title varchar(64) NOT NULL,
-    artist varchar(64) NOT NULL,
-    genre varchar(64) NOT NULL,
-    album varchar(64) NOT NULL,
-    image varchar(512) NOT NULL,
-    CONSTRAINT Sample_pk PRIMARY KEY (id)
-);
+-- --------------------------------------------------------
 
--- Table: User
-CREATE TABLE Users (
-    id int NOT NULL AUTO_INCREMENT,
-    email varchar(256) NOT NULL,
-    login varchar(32) NOT NULL,
-    password varchar(32) NOT NULL,
-    image varchar(512) NOT NULL,
-    games_played int NOT NULL DEFAULT 0,
-    games_won int NOT NULL DEFAULT 0,
-    total_score int NOT NULL DEFAULT 0,
-    is_guest bool NOT NULL,
-    CONSTRAINT User_pk PRIMARY KEY (id)
-);
+--
+-- Structure de la table `games`
+--
 
--- foreign keys
--- Reference: Games_users_Game (table: Games_users)
-ALTER TABLE Games_users ADD CONSTRAINT Games_users_Game FOREIGN KEY Games_users_Game (game_id)
-    REFERENCES Games (id);
+CREATE TABLE `games` (
+  `id` int(11) NOT NULL,
+  `genre` varchar(64) NOT NULL,
+  `current_sample` int(11) NOT NULL,
+  `nb_players` int(11) NOT NULL,
+  `current_first` int(11) DEFAULT NULL,
+  `current_second` int(11) DEFAULT NULL,
+  `current_third` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Reference: Games_users_User (table: Games_users)
-ALTER TABLE Games_users ADD CONSTRAINT Games_users_User FOREIGN KEY Games_users_User (user_id)
-    REFERENCES Users (id);
+-- --------------------------------------------------------
 
--- Reference: games_samples_Game (table: Games_samples)
-ALTER TABLE Games_samples ADD CONSTRAINT games_samples_Game FOREIGN KEY games_samples_Game (game_id)
-    REFERENCES Games (id);
+--
+-- Structure de la table `games_samples`
+--
 
--- Reference: games_samples_Samples (table: Games_samples)
-ALTER TABLE Games_samples ADD CONSTRAINT games_samples_Samples FOREIGN KEY games_samples_Samples (sample_id)
-    REFERENCES Sampled (id);
+CREATE TABLE `games_samples` (
+  `id` int(11) NOT NULL,
+  `sample_id` int(11) NOT NULL,
+  `game_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- End of file.
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `games_users`
+--
+
+CREATE TABLE `games_users` (
+  `id` int(11) NOT NULL,
+  `game_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `score` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `samples`
+--
+
+CREATE TABLE `samples` (
+  `id` int(11) NOT NULL,
+  `title` varchar(64) NOT NULL,
+  `artist` varchar(64) NOT NULL,
+  `genre` varchar(64) NOT NULL,
+  `album` varchar(64) NOT NULL,
+  `image` varchar(512) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(256) NOT NULL,
+  `login` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `image` varchar(512) NOT NULL,
+  `games_played` int(11) NOT NULL DEFAULT '0',
+  `games_won` int(11) NOT NULL DEFAULT '0',
+  `total_score` int(11) NOT NULL DEFAULT '0',
+  `is_guest` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `games`
+--
+ALTER TABLE `games`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `games_samples`
+--
+ALTER TABLE `games_samples`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`),
+  ADD KEY `sample_id` (`sample_id`),
+  ADD KEY `game_id` (`game_id`);
+
+--
+-- Index pour la table `games_users`
+--
+ALTER TABLE `games_users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `games_users_game` (`game_id`),
+  ADD KEY `games_users_user` (`user_id`);
+
+--
+-- Index pour la table `samples`
+--
+ALTER TABLE `samples`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `games`
+--
+ALTER TABLE `games`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `games_samples`
+--
+ALTER TABLE `games_samples`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `games_users`
+--
+ALTER TABLE `games_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `samples`
+--
+ALTER TABLE `samples`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `games_samples`
+--
+ALTER TABLE `games_samples`
+  ADD CONSTRAINT `games_samples_game` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`),
+  ADD CONSTRAINT `games_samples_ibfk_1` FOREIGN KEY (`sample_id`) REFERENCES `samples` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `games_users`
+--
+ALTER TABLE `games_users`
+  ADD CONSTRAINT `games_users_game` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`),
+  ADD CONSTRAINT `games_users_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
